aae18003678922769b97c8ff5d9bf165e9cfbdea
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-02-10 13:10:38 +0800

    PDC03-Module2

---
+++ PDC03MOD02/PDC03MOD02/Activity02.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03MOD02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity02 : ContentPage
+    {
+        public Activity02()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text = args.NewValue.ToString("F3");
+        }
+    }
+}
---
+++ PDC03MOD02/PDC03MOD02/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03MOD02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenActivity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity01());
+        }
+
+        private async void OpenActivity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Activity02());
+        }
+    }
+}
